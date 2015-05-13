251637dab55aaa2b1220de5c4afefdc5671bc947
Nikola Hardi <atlantic777@lugons.org>
2015-05-13 08:39:08 +0200

    JavaDoc - ForecastDataListener documented

--- app/src/main/java/com/ftn/krt/openweathermap/ForecastDataListener.java
+++ app/src/main/java/com/ftn/krt/openweathermap/ForecastDataListener.java
@@ -16,7 +16,10 @@
  *
  * \file ForecastDataListener.java
  * \brief.
- *      This file contains...
+ *      This file contains definition of ForecastDataListener interface
+ *      which is needed for connecting an object that needs to get
+ *      information on forecast data results.
+ *
  * Created on 13.05.2015
  *
  * @Author Nikola Hardi
